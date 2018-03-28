83060e519c6cf45aabd5ce3aa190948d40a6cc8b
angryheng <angryheng@gmail.com>
2018-03-28 10:15:56 +0800

    "add drag.js from branch new1"

--- drag.js
+++ drag.js
@@ -5,5 +5,9 @@ function drag(id) {
     obj.onmousedown = function (e) {
         disX = e.pageX - obj.offsetWidth;
         disY = e.pageY - obj.offsetHeight;
+        document.onmousemove = function (e) {
+            obj.style.left = e.pageX - disX + 'px';
+            obj.style.top = e.pageY - disY + 'px';
+        }
     }
 }
