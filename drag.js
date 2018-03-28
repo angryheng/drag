a3b686381482da861c17cd0a373461cd496748f1
angryheng <angryheng@gmail.com>
2018-03-28 14:04:54 +0800

    "drag successed"

--- drag.js
+++ drag.js
@@ -3,11 +3,16 @@ function drag(id) {
     var disX = 0;
     var disY = 0;
     obj.onmousedown = function (e) {
-        disX = e.pageX - obj.offsetWidth;
-        disY = e.pageY - obj.offsetHeight;
+        disX = e.pageX - obj.offsetLeft;
+        disY = e.pageY - obj.offsetTop;
         document.onmousemove = function (e) {
             obj.style.left = e.pageX - disX + 'px';
             obj.style.top = e.pageY - disY + 'px';
         }
+        document.onmouseup = function () {
+            document.onmousemove = null;
+            document.onmouseup = null;
+        };
     }
+    return false;
 }
