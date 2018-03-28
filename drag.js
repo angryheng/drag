b985d7b597f87ae15955a1ec5576e9e805d60397
angryheng <angryheng@gmail.com>
2018-03-28 09:58:47 +0800

    "add position to demo1.html and change drag.js"

--- drag.js
+++ drag.js
@@ -2,7 +2,8 @@ function drag(id) {
     var obj = document.getElementById(id);
     var disX = 0;
     var dixY = 0;
-    obj.onmousedown = function () {
-        
+    obj.onmousedown = function (e) {
+        disX = e.pageX - obj.offsetWidth;
+        disY = e.pageY - obj.offsetHeight;
     }
 }
